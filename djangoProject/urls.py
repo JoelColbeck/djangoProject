0cc3279606e3e863de74b1a50836526654a8669a
Bashir Arslanaliev <bash.arslanaliev@gmail.com>
2020-10-23 14:02:27 +0300

    Добавлен файл urls.py в приложение news

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 # Импортируем контроллер приложения
-from news.views import *
+# from news.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', index),
-    path('test/', test)
+    path('news/', include('news.urls')),
 ]
